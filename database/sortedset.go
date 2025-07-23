ff8584a85697e65db9fa135e859cb09bcdefd2eb
Numenort <1357897570@qq.com>
2025-07-23 16:51:32 +0800

    累了困了

--- database/sortedset.go
+++ database/sortedset.go
@@ -7,6 +7,9 @@ import (
 	"myredis/interface/myredis"
 	"myredis/protocol"
 	"strconv"
+	"strings"
+
+	"github.com/hashicorp/hcl/v2/ext/tryfunc"
 )
 
 func (db *DB) getAsSortedSet(key string) (*sortedset.SortedSet, protocol.ErrorReply) {
@@ -140,4 +143,258 @@ func execZRevRank(db *DB, args [][]byte) myredis.Reply {
 	}
 
 	return protocol.MakeIntReply(rank)
+}
+
+func execZCard(db *DB, args [][]byte) myredis.Reply {
+	key := string(args[0])
+
+	sortedSet, errReply := db.getAsSortedSet(key)
+	if errReply != nil {
+		return errReply
+	}
+	if sortedSet == nil {
+		return protocol.MakeIntReply(0)
+	}
+
+	return protocol.MakeIntReply(sortedSet.Len())
+}
+
+func range0(db *DB, key string, start int64, end int64, withScores bool, desc bool) myredis.Reply {
+	sortedSet, errReply := db.getAsSortedSet(key)
+	if errReply != nil {
+		return errReply
+	}
+	if sortedSet == nil {
+		return protocol.MakeEmptyMultiBulkReply()
+	}
+
+	size := sortedSet.Len()
+	if start < -1*size {
+		start = 0
+	} else if start < 0 {
+		start = start + size
+	} else if start >= size {
+		return protocol.MakeEmptyMultiBulkReply()
+	}
+
+	if end < -1*size {
+		end = 0
+	} else if end < 0 {
+		end = end + size + 1
+	} else if end < size {
+		end = end + 1
+	} else {
+		end = size
+	}
+	if end < start {
+		end = start
+	}
+
+	slice := sortedSet.RangeByRank(start, end, desc)
+	if withScores {
+		result := make([][]byte, len(slice)*2)
+		i := 0
+		for _, element := range slice {
+			result[i] = []byte(element.Member)
+			i++
+			score := strconv.FormatFloat(element.Score, 'f', -1, 64)
+			result[i] = []byte(score)
+			i++
+		}
+		return protocol.MakeMultiBulkReply(result)
+	}
+	result := make([][]byte, len(slice))
+	i := 0
+	for _, element := range slice {
+		result[i] = []byte(element.Member)
+		i++
+	}
+	return protocol.MakeMultiBulkReply(result)
+}
+
+func execZRange(db *DB, args [][]byte) myredis.Reply {
+	if len(args) != 3 && len(args) != 4 {
+		return protocol.MakeErrReply("ERR wrong number of arguments for 'zrange' command")
+	}
+	withScores := false
+	if len(args) == 4 {
+		if strings.ToUpper(string(args[3])) != "WITHSCORES" {
+			return protocol.MakeErrReply("syntax error")
+		}
+		withScores = true
+	}
+	key := string(args[0])
+	start, err := strconv.ParseInt(string(args[1]), 10, 64)
+	if err != nil {
+		return protocol.MakeErrReply("ERR value is not an integer or out of range")
+	}
+	end, err := strconv.ParseInt(string(args[2]), 10, 64)
+	if err != nil {
+		return protocol.MakeErrReply("ERR value is not an integer or out of range")
+	}
+	return range0(db, key, start, end, withScores, false)
+}
+
+func execZRevRange(db *DB, args [][]byte) myredis.Reply {
+	if len(args) != 3 && len(args) != 4 {
+		return protocol.MakeErrReply("ERR wrong number of arguments for 'zrange' command")
+	}
+	withScores := false
+	if len(args) == 4 {
+		if strings.ToUpper(string(args[3])) != "WITHSCORES" {
+			return protocol.MakeErrReply("syntax error")
+		}
+		withScores = true
+	}
+	key := string(args[0])
+	start, err := strconv.ParseInt(string(args[1]), 10, 64)
+	if err != nil {
+		return protocol.MakeErrReply("ERR value is not an integer or out of range")
+	}
+	end, err := strconv.ParseInt(string(args[2]), 10, 64)
+	if err != nil {
+		return protocol.MakeErrReply("ERR value is not an integer or out of range")
+	}
+	return range0(db, key, start, end, withScores, true)
+}
+
+func execZCount(db *DB, args [][]byte) myredis.Reply {
+	key := string(args[0])
+
+	min, err := sortedset.ParseScoreBorder(string(args[1]))
+	if err != nil {
+		return protocol.MakeErrReply(err.Error())
+	}
+	max, err := sortedset.ParseScoreBorder(string(args[2]))
+	if err != nil {
+		return protocol.MakeErrReply(err.Error())
+	}
+
+	sortedSet, errReply := db.getAsSortedSet(key)
+	if errReply != nil {
+		return errReply
+	}
+	if sortedSet != nil {
+		return protocol.MakeIntReply(0)
+	}
+	return protocol.MakeIntReply(sortedSet.RangeCount(min, max))
+}
+
+func rangeByScore0(db *DB, key string, min SortedSet.Border, max SortedSet.Border, offset int64, limit int64, withScores bool, desc bool) myredis.Reply {
+	sortedSet, errReply := db.getAsSortedSet(key)
+	if errReply != nil {
+		return errReply
+	}
+	if sortedSet == nil {
+		return protocol.MakeEmptyMultiBulkReply()
+	}
+
+	slice := sortedSet.Range(min, max, offset, limit, desc)
+	if withScores {
+		result := make([][]byte, 2*len(slice))
+		i := 0
+		for _, element := range slice {
+			result[i] = []byte(element.Member)
+			i++
+			score := strconv.FormatFloat(element.Score, 'f', -1, 64)
+			result[i] = []byte(score)
+			i++
+		}
+		return protocol.MakeMultiBulkReply(result)
+	}
+	result := make([][]byte, len(slice))
+	i := 0
+	for _, element := range slice {
+		result[i] = []byte(element.Member)
+		i++
+	}
+	return protocol.MakeMultiBulkReply(result)
+}
+
+func execZRangeByScore(db *DB, args [][]byte) myredis.Reply {
+	if len(args) < 3 {
+		return protocol.MakeErrReply("ERR wrong number of arguments for 'zrangebyscore' command")
+	}
+
+	key := string(args[0])
+	min, err := SortedSet.ParseScoreBorder(string(args[1]))
+	if err != nil {
+		return protocol.MakeErrReply(err.Error())
+	}
+	max, err := SortedSet.ParseScoreBorder(string(args[2]))
+	if err != nil {
+		return protocol.MakeErrReply(err.Error())
+	}
+	withScores := false
+	var offset int64 = 0
+	var limit int64 = -1
+
+	if len(args) > 3 {
+		for i := 3; i < len(args); {
+			v := string(args[i])
+			if strings.ToUpper(v) == "WITHSCORES" {
+				withScores = true
+			} else if strings.ToUpper(v) == "LIMIT" {
+				if len(args) < i+3 {
+					return protocol.MakeErrReply("ERR syntax error")
+				}
+				offset, err = strconv.ParseInt(string(args[i+1]), 10, 64)
+				if err != nil {
+					return protocol.MakeErrReply("ERR value is not an integer or out of range")
+				}
+				limit, err = strconv.ParseInt(string(args[i+2]), 10, 64)
+				if err != nil {
+					return protocol.MakeErrReply("ERR value is not an integer or out of range")
+				}
+				i += 3
+			} else {
+				return protocol.MakeErrReply("ERR syntax error")
+			}
+		}
+	}
+	return rangeByScore0(db, key, min, max, offset, limit, withScores, false)
+}
+
+func execZRevRangeByScore(db *DB, args [][]byte) myredis.Reply {
+	if len(args) < 3 {
+		return protocol.MakeErrReply("ERR wrong number of arguments for 'zrangebyscore' command")
+	}
+
+	key := string(args[0])
+	min, err := SortedSet.ParseScoreBorder(string(args[1]))
+	if err != nil {
+		return protocol.MakeErrReply(err.Error())
+	}
+	max, err := SortedSet.ParseScoreBorder(string(args[2]))
+	if err != nil {
+		return protocol.MakeErrReply(err.Error())
+	}
+	withScores := false
+	var offset int64 = 0
+	var limit int64 = -1
+
+	if len(args) > 3 {
+		for i := 3; i < len(args); {
+			v := string(args[i])
+			if strings.ToUpper(v) == "WITHSCORES" {
+				withScores = true
+			} else if strings.ToUpper(v) == "LIMIT" {
+				if len(args) < i+3 {
+					return protocol.MakeErrReply("ERR syntax error")
+				}
+				offset, err = strconv.ParseInt(string(args[i+1]), 10, 64)
+				if err != nil {
+					return protocol.MakeErrReply("ERR value is not an integer or out of range")
+				}
+				limit, err = strconv.ParseInt(string(args[i+2]), 10, 64)
+				if err != nil {
+					return protocol.MakeErrReply("ERR value is not an integer or out of range")
+				}
+				i += 3
+			} else {
+				return protocol.MakeErrReply("ERR syntax error")
+			}
+		}
+	}
+	return rangeByScore0(db, key, min, max, offset, limit, withScores, true)
 }
